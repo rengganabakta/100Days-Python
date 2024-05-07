bbc1bae91c8d1c3cd3678f9eadc14852a7fffa67
rengganabakta <rengganabakta@gmail.com>
2024-05-07 13:52:54 +0700

    delete

--- Day 3/love.py
+++ Day 3/love.py
@@ -4,12 +4,4 @@ name2 = input() # What is their name?
 # 🚨 Don't change the code above 👆
 # Write your code below this line 👇
 
-nama1 = str(len(name1))
-nama2 = str(len(name2))
 
-nama = int(nama1+nama2)
-
-if nama < 10 or nama > 90 :
-    print("Your score is {nama}, you go together like coke and mentos")
-elif nama > 40 and nama < 50:
-    print("Your score is *y*, you are alright together.")
