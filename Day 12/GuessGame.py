bd30a05aaf6a5348901c1f6d986441e0ce6be293
rengganabakta <rengganabakta@example.com>
2024-10-21 01:22:03 +0700

    day 12

---
+++ Day 12/GuessGame.py
@@ -0,0 +1,28 @@
+import random
+
+number = int(random.randint(0, 100))
+trying = 0
+
+        
+print("Welcome, choose dificulity :")
+input1 = input("easy/normal/hard\n")
+
+match input1:
+    case "easy":
+        trying = 10
+    case "normal":
+        trying = 8
+    case "hard":
+        trying = 5
+    case _:
+        print("Invalid difficulty level. Defaulting to normal.")
+        
+for i in range(trying):
+    guess = (int(input(f"{i} guess is ? =>>  ")))
+    if guess == number:
+        print("You win")
+        break
+    elif guess > number:
+        print("Lower...")
+    elif guess < number:
+        print("Higher...")
